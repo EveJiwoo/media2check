dfe35a173f1a2b6305c46fc6bfde46241bac5bd3
Evelyn Jiwoo Lee <145739444+media2check@users.noreply.github.com>
2023-09-28 17:43:59 +0900

    Update and rename uploader.py to originalpost.py

--- originalpost.py
+++ originalpost.py
@@ -1,12 +1,13 @@
+#Using Twitter(Currently X) API, we can find origianl post uploader
 import tweepy
 
-#Twitter API credentials to access (not shown here for 
+#Twitter credentials to access API(not shown here for security reason)
 consumer_key = 'our_key'
-consumer_secret = 'consumer_secret'
-access_token = 'access_token'
-access_token_secret = 'access_token_secret'
+consumer_secret = 'our_consumer_secret'
+access_token = 'our_access_token'
+access_token_secret = 'our_access_token_secret'
 
-# Authenticate with the Twitter API
+# Authenticate process for Twitter API
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
