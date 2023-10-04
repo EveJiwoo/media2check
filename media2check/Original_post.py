a11fc6c1e8824e19bcee993912f1d4b9b5bf5d3c
Evelyn Jiwoo Lee <145739444+EveJiwoo@users.noreply.github.com>
2023-10-05 03:30:55 +0900

    Update Original_post.py

--- media2check/Original_post.py
+++ media2check/Original_post.py
@@ -2,12 +2,10 @@
 
 # Function to search for the original post by a specific user
 def find_original_post(screen_name, keyword):
-    # Search for tweets containing the keyword
     tweets = api.search(q=keyword, lang="en", result_type="recent")
 
     for tweet in tweets:
         if tweet.user.screen_name == screen_name:
-            # This tweet was posted by the user you're interested in
             return tweet
 
     return None
