4fe18350fb1b4396e0f20d80ee568d8032faaa8f
Evelyn Jiwoo Lee <145739444+EveJiwoo@users.noreply.github.com>
2023-10-05 18:19:10 +0900

    Update Original_post.py

--- media2check/Original_post.py
+++ media2check/Original_post.py
@@ -1,19 +1,22 @@
 # Twitter API tweepy used, credential and authenticate here removed for security reasons
 
-# Function to search for the original post by a specific user
-def find_original_post(screen_name, keyword):
-    tweets = api.search(q=keyword, lang="en", result_type="recent")
+# Replace 'retweet_id' with the ID of the retweet you want to find the original tweet for
+retweet_id = 'your_retweet_id_here'
 
-    for tweet in tweets:
-        if tweet.user.screen_name == screen_name:
-            return tweet
+try:
+    # Get the original tweet and the retweet using the retweet ID
+    retweet = api.get_status(id=retweet_id, tweet_mode='extended', include_entities=True)
 
-    return None
-
-# Tracking original tweet information
-original_tweet = find_original_post('original_account_name', 'your_keyword')
-
-if original_tweet:
-    print("Original post by @{original_tweet.user.screen_name}: {original_tweet.text}")
-else:
-    print("Original post not found.")
+    # Check if the tweet is a retweet; if so, find the original tweet
+    if hasattr(retweet, 'retweeted_status'):
+        original_tweet = retweet.retweeted_status
+        print("Original Tweet Text:", original_tweet.full_text)
+        print("Original Tweet User:", original_tweet.user.screen_name)
+        print("Retweet Text:", retweet.full_text)
+        print("Retweet User:", retweet.user.screen_name)
+    else:
+        # If the tweet is not a retweet, it is already the original tweet
+        print("Original Tweet Text:", retweet.full_text)
+        print("Original Tweet User:", retweet.user.screen_name)
+except tweepy.TweepError as e:
+    print("Error: ", str(e))
