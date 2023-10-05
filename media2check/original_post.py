c3de07f12ac27a33cb3f0dbe08f0afe451cd3f6a
Evelyn Jiwoo Lee <145739444+EveJiwoo@users.noreply.github.com>
2023-10-05 22:30:07 +0900

    Update original_post.py

--- media2check/original_post.py
+++ media2check/original_post.py
@@ -3,21 +3,23 @@
 
 # Insert twitter ID for checkup on 'id_to_check' to find the original tweet
 retweet_id = 'id_to_check'
+def get_original_post:
+    try:
+        # Get the original tweet and the retweet using the retweet ID
+        retweet = api.get_status(id=retweet_id, tweet_mode='extended', include_entities=True)
+    
+        # Check if the tweet is a retweet; if so, find the original tweet
+        if hasattr(retweet, 'retweeted_status'):
+            original_tweet = retweet.retweeted_status
+            print("Original Tweet Text:", original_tweet.full_text)
+            print("Original Tweet User:", original_tweet.user.screen_name)
+            print("Retweet Text:", retweet.full_text)
+            print("Retweet User:", retweet.user.screen_name)
+        else:
+            # If the tweet is not a retweet, it is already the original tweet
+            print("Original Tweet Text:", retweet.full_text)
+            print("Original Tweet User:", retweet.user.screen_name)
+    except tweepy.TweepError as e:
+        print("Error: ", str(e))
 
-try:
-    # Get the original tweet and the retweet using the retweet ID
-    retweet = api.get_status(id=retweet_id, tweet_mode='extended', include_entities=True)
-
-    # Check if the tweet is a retweet; if so, find the original tweet
-    if hasattr(retweet, 'retweeted_status'):
-        original_tweet = retweet.retweeted_status
-        print("Original Tweet Text:", original_tweet.full_text)
-        print("Original Tweet User:", original_tweet.user.screen_name)
-        print("Retweet Text:", retweet.full_text)
-        print("Retweet User:", retweet.user.screen_name)
-    else:
-        # If the tweet is not a retweet, it is already the original tweet
-        print("Original Tweet Text:", retweet.full_text)
-        print("Original Tweet User:", retweet.user.screen_name)
-except tweepy.TweepError as e:
-    print("Error: ", str(e))
+    return retweet
