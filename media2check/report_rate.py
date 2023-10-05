e180695b66ce95b4cd4fd8496cbddb47b4e093e3
Evelyn Jiwoo Lee <145739444+EveJiwoo@users.noreply.github.com>
2023-10-05 23:03:02 +0900

    Create report_rate.py

---
+++ media2check/report_rate.py
@@ -0,0 +1,14 @@
+def check_report_rate(tweet_id):
+    try:
+        tweet = api.get_status(tweet_id)
+        num_reports = tweet.user_reports + tweet.reports # Get the number of reports
+        total_interactions = tweet.favorite_count + tweet.retweet_count + tweet.reply_count # Calculate total interactions
+        
+      # Calculate the report rate as a percentage
+        report_rate = (num_reports / total_interactions)  
+      
+
+        return report_rate > .1  # Check if the report rate is greater than 10%
+    except tweepy.TweepError as e:
+        print(f"Error: {e}")
+        return False
