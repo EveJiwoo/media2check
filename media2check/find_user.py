0a3ace960afbcaf4e565cb8c9e99d9d63523d874
Evelyn Jiwoo Lee <145739444+EveJiwoo@users.noreply.github.com>
2023-10-06 00:04:13 +0900

    Update find_user.py

--- media2check/find_user.py
+++ media2check/find_user.py
@@ -1,22 +1,13 @@
 #Function to find user id from tweet id
 # Twitter API tweepy used, credential and authenticate here removed for security reasons
 
-def get_original_poster_user_id(tweet_id):
+def get_user_id_from_tweet_id(tweet_id):
     try:
+        # Get the tweet object
         tweet = api.get_status(tweet_id)
-        user_id = tweet.user.id_str  # Get the user's ID as a string
+        # Extract the user ID from the tweet
+        user_id = tweet.user.id_str
         return user_id
     except tweepy.TweepError as e:
         print(f"Error: {e}")
         return None
-
-# Input the tweet ID
-tweet_id = input("Enter the tweet ID: ")
-
-# Get the user ID of the original poster of the tweet
-user_id = get_original_poster_user_id(tweet_id)
-
-if user_id:
-    print(f"The user ID of the original poster is: {user_id}")
-else:
-    print("Unable to retrieve the user ID.")
