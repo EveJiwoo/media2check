6022d60c8de2f68b567cc17365889ced77309d83
Evelyn Jiwoo Lee <145739444+EveJiwoo@users.noreply.github.com>
2023-10-17 18:53:34 -0400

    Update upload_interval.py

--- media2check/upload_interval.py
+++ media2check/upload_interval.py
@@ -1,5 +1,5 @@
 # Function to find user's most recent 10 post upload times and interval
-# Twitter API tweepy used, credential and authenticate here removed for security reasons
+# (Twitter API tweepy used), Removing credential and authenticate for security reasons
 import tweepy
 from datetime import datetime
 
