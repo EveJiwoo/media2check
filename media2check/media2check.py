de5bc094c5434685659d51e129f558c90d016ef7
Evelyn Jiwoo Lee <145739444+EveJiwoo@users.noreply.github.com>
2023-10-17 18:52:34 -0400

    Update media2check.py

--- media2check/media2check.py
+++ media2check/media2check.py
@@ -7,18 +7,19 @@ from propagation_layer import get_propagation_layer
     but some platforms do not provide API to access their data and we also
     have excluded platforms using image and video for future development.
 
-    Twitter(currently X) is text-based platform which we had access and 
-    were able to develop functions. Below is our approach to filter fake news.
+    Twitter(currently X) is text-based platform which we had access to and 
+    were able to develop functions. Down below is our approach to distingush 
+    fake news from reliable news.
 
     Prioritizing SNS posts to review will minimize resource input, thus M2C
     chose trending posts. 
 
-    Then using user report system, if report rate is over 10% its up for
-    review. Original post uploader is found and user is reviewed. API is 
-    not provided by Twitter for this function so we assumed it will be provided 
-    later. 
+    Then using user report system, if report rate is over 10%, it is processed for
+    review. In this step, the original post uploader is found and user is reviewed. 
+    API is not provided by Twitter for this function, so we assumed it will be 
+    provided later. 
 
-    User credibility is checked by credit scoring model and added bot upload 
+    User credibility is checked by the credit scoring model and added bot upload 
     detection method by calculating upload time interval.
 
     Spread pattern is checked by looking at layers and number of retweets.
@@ -29,17 +30,17 @@ from propagation_layer import get_propagation_layer
 
 def start():  # pylint: disable=too-many-return-statements,too-many-branches
 
-    # Get the top trending topics tweets
+    # Getting the top trending topics tweets
     trending_tweet_ids = trending_tweets()
    
-    # show trending tweet's id and text
+    # Showing the trending tweet's id and text
     for i, (tweet_id, tweet_text) in enumerate(trending_tweets):
     print(f"{i + 1}. Tweet ID: {tweet_id}, Tweet Text: {tweet_text}")
 
-    # find tweet id of report rate over 10% 
+    # Finding tweet id of report rate over 10% 
     suspicious_tweets  = check_report_rate(tweet_id)
 
-    # find original uploader of trending posts
+    # Finding original uploader of trending posts
     original_upload = get_original_post_user_id(suspicious_tweets)
     print original_upload
 
